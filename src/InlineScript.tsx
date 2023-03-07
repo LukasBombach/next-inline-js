1aa02247a06ffe00edc72997622f5a4cc6ebe041
Lukas Bombach <lukas.bombach@googlemail.com>
2023-03-07 17:42:30 +0100

    wip

--- src/InlineScript.tsx
+++ src/InlineScript.tsx
@@ -1,10 +1,11 @@
 import type { FC } from "react";
 
-type InlineScriptProps = Omit<JSX.IntrinsicElements["script"], "src" | "dangerouslySetInnerHTML"> & {
-  src: Promise<any>;
-};
+type ScriptElementProps = Omit<JSX.IntrinsicElements["script"], "src" | "dangerouslySetInnerHTML">;
 
-export const InlineScript: FC<InlineScriptProps> = ({ src, ...props }) => {
-  debugger;
+type InlineScriptProps<Src extends Promise<any>, Props = Src extends Promise<infer T> ? T : never> = {
+  src: Src;
+} & Props;
+
+export const InlineScript = <S extends Promise<any>>({ src, ...props }: InlineScriptProps<S>) => {
   return <script {...props} dangerouslySetInnerHTML={{ __html: "console.log('InlineScript');" }} />;
 };
