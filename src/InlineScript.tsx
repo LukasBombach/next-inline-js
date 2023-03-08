ba08671ce0d12c1812353a9c0b079e5d47993986
Lukas Bombach <lukas.bombach@googlemail.com>
2023-03-08 08:38:42 +0100

    next dynamic wip

--- src/InlineScript.tsx
+++ src/InlineScript.tsx
@@ -1,7 +1,5 @@
 import dynamic from "next/dynamic";
 
-import type { FC } from "react";
-
 type ScriptElementProps = Omit<JSX.IntrinsicElements["script"], "src" | "dangerouslySetInnerHTML">;
 
 type InlineScriptProps<
@@ -12,5 +10,10 @@ type InlineScriptProps<
 } & Props;
 
 export const InlineScript = <S extends Promise<any>>({ src, ...props }: InlineScriptProps<S>) => {
-  return <script {...props} dangerouslySetInnerHTML={{ __html: src }} />;
+  return dynamic(() =>
+    src.then(code => {
+      const Script = () => <script {...props} dangerouslySetInnerHTML={{ __html: code }} />;
+      return Script;
+    })
+  );
 };
