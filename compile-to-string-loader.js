b0310e4ba2bc01620a9ae01699726b6626edd5dc
Lukas Bombach <lukas.bombach@googlemail.com>
2023-03-09 20:15:50 +0100

    no idea

--- compile-to-string-loader.js
+++ compile-to-string-loader.js
@@ -1,24 +1,17 @@
-const { getRemainingRequest } = require("loader-utils");
+const path = require("node:path");
 
 /**
  * @type {import('webpack').LoaderDefinitionFunction}
  */
 module.exports = function compileToStringLoader(content, map, meta) {
   const callback = this.async();
-  const loaderContext = this;
-
-  if (/NOPE/.test(this.request)) {
-    callback(null, content);
-    return;
-  }
-
-  debugger;
-
-  this.importModule(this.request + "?NOPE").then(source => {
-    console.log(loaderContext);
-    callback(null, `module.exports = ${JSON.stringify(source)};`);
-    setTimeout(() => {
-      debugger;
-    }, 0);
-  });
+  this._compilation.addEntry(
+    this.context,
+    path.relative(this.context, this.resourcePath),
+    { name: "test" },
+    (err, result) => {
+      console.log("addEntry", err, result);
+      callback(null, content);
+    }
+  );
 };
