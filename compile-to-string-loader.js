7626b4493d62fde45154cca4c110f9e9532a81d6
Lukas Bombach <lukas.bombach@googlemail.com>
2023-03-14 17:26:48 +0100

    WIP

--- compile-to-string-loader.js
+++ compile-to-string-loader.js
@@ -1,9 +1,26 @@
+const path = require("node:path");
+
 /**
  * @this {import('webpack').LoaderContext<{}>}
  * @type {import('webpack').LoaderDefinitionFunction}
  */
 function compileToStringLoader(content, sourceMap, additionalData) {
   const callback = this.async();
+
+  const webpack = this._compiler.webpack;
+  const compiler = this._compiler;
+  const context = compiler.context;
+  const entry = path.relative(context, this.resourcePath);
+
+  new webpack.EntryPlugin(context, entry, {
+    name: "my-entry-test-name",
+    filename: "my-entry-test-filename",
+  }).apply(compiler);
+
+  this.addDependency(this.resourcePath);
+
+  debugger;
+
   callback(null, content, sourceMap, additionalData);
 }
 
